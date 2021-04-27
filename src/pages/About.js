21c8ba5cbe062f108ef4eccf58f1c926997c34e4
Vikat Rane <vikat9596@gmail.com>
2021-04-27 06:32:23 +0530

    initial commit 2.0

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>Designed & Developed by Vcut ✌️</p>
+    </section>
+  )
+}
+
+export default About
